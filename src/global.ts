58b40b033abf0c584861d72672747f8320f64fdc
wangyuan0108 <870147852@qq.com>
2021-03-08 18:00:46 +0800

    获取code

--- src/global.ts
+++ src/global.ts
@@ -1,10 +1,32 @@
 /*
  * @Author: wangyuan
  * @Date: 2021-03-01 15:12:01
- * @LastEditTime: 2021-03-02 10:02:16
+ * @LastEditTime: 2021-03-08 17:59:09
  * @LastEditors: wangyuan
  * @Description:
  */
 import 'amfe-flexible/index';
 import 'normalize.css';
 window.wx = require('weixin-js-sdk');
+interface IQuery {
+  [propName: string]: any;
+}
+
+const getUrlQuery: () => IQuery = () => {
+  const url = decodeURI(location.search);
+  let queryObj: IQuery = {};
+  if (url.indexOf('?') != -1) {
+    let str = url.substr(1);
+    if (str.indexOf('&') != -1) {
+      let strArr = str.split('&');
+      strArr.forEach((item) => {
+        let itemArr = item.split('=');
+        queryObj[itemArr[0]] = itemArr[1];
+      });
+    } else {
+      let itemArr = str.split('=');
+      queryObj[itemArr[0]] = itemArr[1];
+    }
+  }
+  return queryObj;
+};
